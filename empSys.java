97bef7cc10e7f2b5a69211e44910c76b94ed1f63
aryan1403 <aaryan14032006@gmail.com>
2024-04-16 14:41:47 +0530

    set and map

--- empSys.java
+++ empSys.java
@@ -1,15 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class empSys {
     public static void main(String[] args) {
-        emp e1 = new emp("Aaryan", "Trainer", 100);
-        emp e2 = new emp("Arush", "Developer", 1000);
-        emp e3 = new emp("Ananya", "Trainee", 200);
+        emp e1 = new emp(UUID.randomUUID().toString(), "Aaryan", "Trainer", 100);
+        emp e2 = new emp(UUID.randomUUID().toString(), "Arush", "Developer", 1000);
+        emp e3 = new emp(UUID.randomUUID().toString(), "Ananya", "Trainee", 200);
         List<emp> list = new ArrayList<>();
         list.add(e1);
         list.add(e2);
         list.add(e3);
+
+        Set<String> sid = list.stream().map(e -> e.id).collect(Collectors.toSet());
+        sid.forEach(System.out::println);
 
         // TODO: Filter emp where salary > 100
         // list.stream().filter(e -> e.role.equals("Trainer")).forEach(System.out::println);
@@ -26,11 +32,13 @@ public class empSys {
 
 // Model
 class emp {
+    String id;
     String name;
     String role;
     double salary;
 
-    public emp(String name, String role, double salary) {
+    public emp(String id, String name, String role, double salary) {
+        this.id = id;
         this.name = name;
         this.role = role;
         this.salary = salary;
@@ -38,6 +46,6 @@ class emp {
 
     @Override
     public String toString() {
-        return "Emp: {" + name + ", " + role + ", " + salary + "}";
+        return "Emp: {" + id + ", " + name + ", " + role + ", " + salary + "}";
     }
 }
