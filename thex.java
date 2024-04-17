67e744414cf09faf5459883cc788213ab9833f23
aryan1403 <aaryan14032006@gmail.com>
2024-04-17 16:25:49 +0530

    thread sleep

--- thex.java
+++ thex.java
@@ -4,6 +4,11 @@ public class thex {
             @Override
             public void run() {
                 for (int i = 0; i < 100; i++) {
+                    try {
+                        Thread.sleep(200); 
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     System.out.println(i);
                 }
             }
@@ -13,6 +18,11 @@ public class thex {
             public void run() {
                 for (int i = 0; i < 100; i++) {
                     System.out.println(i * -1);
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         };
