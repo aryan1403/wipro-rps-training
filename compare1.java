141141bbe16222ec5621a912f79fda7329233f86
aryan1403 <aaryan14032006@gmail.com>
2024-04-17 11:32:29 +0530

    updated

--- compare1.java
+++ compare1.java
@@ -1,41 +1,63 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class compare1 {
     public static void main(String[] args) {
         List<Student> list = new ArrayList<>();
-        list.add(new Student(1, "Arush"));
-        list.add(new Student(0, "Aaryan"));
+        list.add(new Student(1, "Arush", new Marks(90, 95, 62)));
+        list.add(new Student(0, "Aaryan", new Marks(91, 100, 63)));
 
-        Collections.sort(list, (s1, s2) -> {
-            if(s1.rollno < s2.rollno) {
+        /* Collections.sort(list, (s1, s2) -> {
+            if(s1.marks.avg() < s2.marks.avg()) {
                 return -1; // swap
             }
             return 0;
-        });
+        }); */
         Student maxStudent = list.stream().max((s1, s2) -> {
-            if(s1.rollno < s2.rollno) {
+            if(s1.marks.avg() < s2.marks.avg()) {
                 return -1; // swap
             }
             return 0;
         }).get();
-        System.out.println("Highest rollno : " + maxStudent.name);
-        list.forEach(System.out::println);
+        System.out.println("Highest marks : " + maxStudent.name);
+        // list.forEach(System.out::println);
     }
 }
 
 class Student {
     int rollno;
     String name;
+    Marks marks;
 
-    Student(int rollno, String name) {
+    Student(int rollno, String name, Marks marks) {
         this.rollno = rollno;
         this.name = name;
+        this.marks = marks;
     }
 
     @Override
     public String toString() {
         return "Student: " + name + ", " + rollno;
+    }
+}
+
+class Marks  {
+    int pmarks;
+    int cmarks;
+    int mmarks;
+
+    public Marks(int pmarks, int cmarks, int mmarks) {
+        this.pmarks = pmarks;
+        this.cmarks = cmarks;
+        this.mmarks = mmarks;
+    }
+
+    public double avg() {
+        return (pmarks + cmarks + mmarks) / 3;
+    }
+
+    @Override
+    public String toString() {
+        return "Marks [pmarks=" + pmarks + ", cmarks=" + cmarks + ", mmarks=" + mmarks + "]";
     }
 }
