afcfc2d8c237d553443e172aad201432a498d437
aryan1403 <aaryan14032006@gmail.com>
2024-04-25 10:51:13 +0530

    update

--- app/src/main/java/com/example/demoapp/MainActivity.java
+++ app/src/main/java/com/example/demoapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.demoapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -17,12 +18,36 @@ import com.example.demoapp.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btn;
+    EditText name, pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn1);
+        btn.setOnClickListener(this);
+
+        name = findViewById(R.id.namebox);
+        pass = findViewById(R.id.passbox);
+
+    }
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onClick(View view) {
+        if(view == btn) {
+            String nt = name.getText().toString();
+            String pt = pass.getText().toString();
+
+            if(nt.equals("aryan") && pt.equals("123")) {
+                // logged in successfully
+            } else {
+
+            }
+        }
     }
 }
